bad52bb574f91d6a78288c6647357df1825047f8
satish patil <satish patil>
2016-05-02 09:30:17 +0530

    added image to the home screen need to implement search criteria with search button

---
+++ app/scripts/controllers/homeScreenS1.js
@@ -0,0 +1,29 @@
+'use strict';
+
+
+// angular.module('testApp')
+// .controller('MainCtrl', function($scope){
+
+//     $scope.firstName="ASASAS";
+//     $scope.lastName="deep";
+//     });
+
+
+// angular.module('testApp')
+// .controller('nameslist', function($scope){
+//     $scope.names=[
+//         {name1:'satish', job:'frientEnddeveloper'},
+//         {name1:'Raju', job:'frientEnddeveloper'},
+//         {name1:'Ramesh', job:'developer'},
+//         {name1:'amar', job:'tester'},
+
+//     ]
+// });
+
+
+angular.module('3TttApp')
+.controller('homeScreen',function($scope){
+	$scope.imageSource1='images/OnlineTraining.jpg';
+
+
+});
