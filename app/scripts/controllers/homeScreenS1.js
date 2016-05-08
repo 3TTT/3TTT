44deed5c442bcff120fd407ac27b14493aab3ecc
satish patil <satish patil>
2016-05-08 17:18:09 +0530

    added a div with COurse details and displayed course image and Details

--- app/scripts/controllers/homeScreenS1.js
+++ app/scripts/controllers/homeScreenS1.js
@@ -1,25 +1,6 @@
 'use strict';
 
 
-// angular.module('testApp')
-// .controller('MainCtrl', function($scope){
-
-//     $scope.firstName="ASASAS";
-//     $scope.lastName="deep";
-//     });
-
-
-// angular.module('testApp')
-// .controller('nameslist', function($scope){
-//     $scope.names=[
-//         {name1:'satish', job:'frientEnddeveloper'},
-//         {name1:'Raju', job:'frientEnddeveloper'},
-//         {name1:'Ramesh', job:'developer'},
-//         {name1:'amar', job:'tester'},
-
-//     ]
-// });
-
 
 angular.module('3TttApp')
 .controller('homeScreen',function($scope, $http) {
@@ -44,6 +25,15 @@ angular.module('3TttApp')
   		}
   	});
 };
+
+$scope.CourseImage='images/javaCourseImage.jpg';
+
+$http.get('scripts/controllers/courseDetailsResponse.json').success(function(response){
+  $scope.CourseDetailsjson = response;
+  console.log('$scope.CourseDetailsjson',$scope.CourseDetailsjson );
+}).error(function(error){
+  console.log("error", error);
+});
 
 
 });
