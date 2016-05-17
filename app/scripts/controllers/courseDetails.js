cb778db53c3442aced2f1b2c6436933328515f2d
satish patil <satish patil>
2016-05-17 08:08:22 +0530

    enroll course logic implemented

--- app/scripts/controllers/courseDetails.js
+++ app/scripts/controllers/courseDetails.js
@@ -8,5 +8,20 @@
  * Controller of the 3TttApp
  */
 angular.module('3TttApp')
-  .controller('courseDetails', function () {
+  .controller('courseDetails', function ($scope) {
+
+
+  	$scope.studentList = 0;
+  	$scope.listOfEnrolledStudents = function(count){
+  	// 	if(count<2){
+  	// 	$scope.studentList +=count;
+  	// }else{
+  	// 	console.log("only 6 number per class, please enroll in next call");
+  	// 	// <p>only 6 number per class, please enroll in next call<p>
+  	// 	$scope.studentList +=count+4;
+  	// }
+
+  	$scope.studentList +=count;
+
+  	};
   });
