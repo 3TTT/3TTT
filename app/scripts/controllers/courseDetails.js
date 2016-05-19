679f5f88c91d752c72591cbabdd233e0c224db8c
satish patil <satish patil>
2016-05-19 10:20:20 +0530

    updated json

--- app/scripts/controllers/courseDetails.js
+++ app/scripts/controllers/courseDetails.js
@@ -8,12 +8,19 @@
  * Controller of the 3TttApp
  */
 angular.module('3TttApp')
-  .controller('courseDetails', function ($scope) {
+  .controller('courseDetails', function ($scope, $http) {
 
 
   	$scope.studentList = 0;
   	$scope.listOfEnrolledStudents = function(count){
   	$scope.studentList +=count;
   	};
+
+  	$http.get('scripts/controllers/courseDetailsResponse.json').success(function(response){
+  $scope.CourseDetailsjson = response;
+  console.log('$scope.CourseDetailsjson',$scope.CourseDetailsjson );
+}).error(function(error){
+  console.log("error", error);
+});
 
   });
